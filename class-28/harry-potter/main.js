ae3df78b0def09abdbd97fa2fc4af8525a611e3e
Lillian Nguyen <lillianxnguyen@gmail.com>
2024-11-16 21:31:58 -0600

    change crest row to column on click

--- class-28/harry-potter/main.js
+++ class-28/harry-potter/main.js
@@ -5,7 +5,19 @@ let crests = document.querySelectorAll('.crest');
 crests.forEach(crest => {
     crest.addEventListener('mouseover', fadeCrests);
     crest.addEventListener('mouseout', showCrests);
-    crest.addEventListener('click', checkClick)
+    //click which house is clicked and assign to variable
+    crest.addEventListener('click', (event) => {
+        let clickedCrest = event.currentTarget.getAttribute('id');
+        let currentCrest = ''
+
+        crests.forEach(crest => {
+            //clicking a crest adds the flex-direction: column property
+            document.querySelector('.house-container').classList.add('columnCrests');
+
+            clickedCrest == 'gryffindor' ? currentCrest = 'gryffindor' : clickedCrest == 'ravenclaw' ? currentCrest = 'ravenclaw' : clickedCrest == 'slytherin' ? currentCrest = 'slytherin' : currentCrest = 'hufflepuff';
+        })
+        runHouse(currentCrest)
+    })
 })
 
 //change opacity when hovered / revert to original opacity when leaving hover
@@ -28,41 +40,30 @@ function showCrests() {
     })
 }
 
-//click which house is clicked and assign to variable
-function checkClick(event) {
-    //get clicked element
-    let clickedCrest = event.target;
-
-    if (clickedCrest == document.getElementById('gryffindor')) {
-        currentCrest = 'gryffindor';
-        console.log(currentCrest)
-    }
-
-    runHouse();
-}
-
 function runHouse(currentCrest) {
     fetch(`https://hp-api.herokuapp.com/api/characters/house/${currentCrest}`)
         .then(res => res.json()) //parse response as JSON
         .then(data => {
-            checkClick()
             console.log(data)
         })
         .catch(err => {
             console.log(`error ${err}`);
         });
-
 }
-
 
 /*
 
 CURRENTLY WORKING ON:
-- getting data back for characters via checkclick/runhouse functions
+- resize and reposition column crests
+- pull name and img data 
+- get backup photo if img blank
 
 OBJECTIVES:
+- create container for house photos
+- add classes that rearrange flex to flex column and position to the left side on click 
 - change opacity of other house crests after clicking on one
 
 COMPLETED:
 - 11/14/24: changing opacity of other houses on hover
+- 11/15/24: getting data back for characters via checkclick/runhouse functions
 */

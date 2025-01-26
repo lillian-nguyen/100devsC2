495b8064e5c8676740be4b6ff1adef660cc7ab18
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-01-25 18:27:53 -0600

    update and show quotes from database

--- class-38/zell-CRUD/server.js
+++ class-38/zell-CRUD/server.js
@@ -3,28 +3,47 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 
-//place this before CRUD handlers (express middleware)
-app.use(express.urlencoded({ extended: true }))
-
-//create server that browsers can connect to with Express' listen method
-app.listen(3000, function () {
-    console.log('listening on 3000')
-});
-
-//app.get(endpoint, callback);
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-    console.log(__dirname);
-})
-
-//handle POST request w/ post method in server.js (value placed in action attribute for form)
-app.post('/quotes', (req, res) => {
-    console.log(req.body);
-});
-
 MongoClient.connect('mongodb+srv://Yoda:yodaman@cluster0.hl4oc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(client => {
     console.log('Connected to Database');
     const db = client.db('star-wars-quotes');
+    const quotesCollection = db.collection('quotes');
+
+    //set view engine to ejs (tells express we're using EJS as template engine) goes before any app methods
+    app.set('view engine', 'ejs')
+
+    //place this before CRUD handlers (express middleware)
+    app.use(express.urlencoded({ extended: true }))
+
+    //app.get(endpoint, callback);
+    app.get('/', (req, res) => {
+
+        const cursor = db.collection('quotes').find();
+        console.log(cursor);
+
+        db.collection('quotes')
+            .find()
+            .toArray()
+            .then(results => {
+                res.render('index.ejs', { quotes: results })
+            })
+            .catch(error => console.error(error))
+    })
+
+    //handle POST request w/ post method in server.js (value placed in action attribute for form)
+    app.post('/quotes', (req, res) => {
+        quotesCollection
+            .insertOne(req.body)
+            .then(result => {
+                res.redirect('/')
+            })
+            .catch(error => console.log(error))
+    });
+
+    //create server that browsers can connect to with Express' listen method
+    app.listen(3000, function () {
+        console.log('listening on 3000')
+    });
+
 }
 
 ).catch(error => console.log(error))

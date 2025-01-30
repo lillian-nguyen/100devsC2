73609d971a05cdb5f45e8ceaa38da6fd8be9d4f8
Lillian Nguyen <lillianxnguyen@gmail.com>
2025-01-29 23:09:10 -0600

    basic styling for coin flip game

--- class-38/coin-flip/js/main.js
+++ class-38/coin-flip/js/main.js
@@ -7,22 +7,52 @@ headAndTails.forEach(button => {
     button.addEventListener('click', iniateGuess)
 })
 
-//button styling on head or tail selection + assign variable for playerGuess
+//Let user guess heads/tails and show step two after player choice made
 function iniateGuess(event) {
+    //step 1: button styling on head or tail selection + assign variable for playerGuess
+
     let playerGuess = '';
 
-    if (heads == event.target) {
+    if (heads === event.target) {
         heads.style.color = 'white';
-        heads.style.background = 'blue';
+        heads.style.background = '#6D7969';
         tails.style.color = 'black';
         tails.style.background = 'white';
-        playerGuess = 'heads'
+        playerGuess = 'heads';
     }
     else if (tails == event.target) {
         tails.style.color = 'white';
-        tails.style.background = 'blue';
+        tails.style.background = '#6D7969';
         heads.style.color = 'black';
         heads.style.background = 'white';
-        playerGuess = 'tails'
+        playerGuess = 'tails';
     }
+    //show step 2
+    document.querySelector('.step2').style.visibility = 'visible';
+}
+
+//make request to api when flip coin button clicked
+let flipButton = document.querySelector('#flip-button');
+
+flipButton.addEventListener('click', showResult);
+flipButton.addEventListener('click', showScore);
+
+function showScore() {
+    document.querySelector('.player').style.visibility = 'visible';
+    document.querySelector('.computer').style.visibility = 'visible';
+}
+
+async function showResult(playerGuess) {
+    // console.log(playerGuess);
+    const coinOption = playerGuess;
+    console.log(coinOption);
+    const res = await fetch(`/api?guess=${coinOption}`);
+    const data = await res.json();
+
+    console.log(data);
+
+    //**fill in result? coin and computer and player score 
+    let coinStatus = document.querySelector('#coin-status');
+    let playerScore = document.querySelector('.player-box');
+    let computerScore = document.querySelector('.computer-box');
 }
--- class-38/coin-flip/server.js
+++ class-38/coin-flip/server.js
@@ -18,12 +18,32 @@ const server = http.createServer((req, res) => {
             res.write(data);
             res.end();
         })
-    } else if (page == 'js/main.js') {
+    } else if (page == '/js/main.js') {
         fs.readFile('js/main.js', function (err, data) {
             res.writeHead(200, { 'Content-Type': 'text/javascript' });
             res.write(data);
             res.end();
         })
+    }
+    else if (page == '/api') {
+        if ('guess' in params) {
+            if (params['guess'] == 'heads') {
+
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(objToJson))
+                const coinOption = params['guess']; // ??? 
+                const flipResult = Math.ceil(Math.random() * 2) === 1 ? 'heads' : 'tails';
+
+                console.log(`Player Guess: ${coinOption}, Flip Result: ${flipResult}`)
+
+                const correct = coinOption == flipResult; //???
+
+                const objToJson = {
+                    result: flipResult
+                }
+
+            }
+        }
     }
 })
 
